eccc900da72a0aaab6bde66a3e4b9ce5391b65cb
dpaweloszek <dawidpaweloszek99@gmail.com>
2022-03-21 16:29:45 +0100

    Fixed issue with duped .sln files

--- tpum/tpum/Program.cs
+++ tpum/tpum/Program.cs
@@ -1,20 +1,12 @@
 ﻿using System;
 
-namespace TPUM
+namespace tpum
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
-    }
-
-    public class Rooter
-    {
-        public double SquareRoot(double input)
-        {
-            return input / 2;
         }
     }
 }
